e12894a13b368167cb0c2ece51299c37ad3d1656
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2024-04-04 11:41:08 +0200

    setting type module

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/nextjs-dashboard",
-  output: "export",
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
