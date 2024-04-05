4d3e3628cb6630c8b6c22765607bb77ce1ccb61d
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2024-04-05 17:19:30 +0200

    added metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,16 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+import { 
+  CardsSkeleton, 
+  LatestInvoicesSkeleton, 
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
  
 export default async function Page() {
   return (
@@ -12,7 +21,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardSkeleton/>}>
+        <Suspense fallback={<CardsSkeleton/>}>
           <CardWrapper />
         </Suspense>
       </div>
--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
@@ -24,19 +24,19 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
-
+ 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
-
           if (!user) return null;
-
           const passwordsMatch = await bcrypt.compare(password, user.password);
  
           if (passwordsMatch) return user;
         }
  
+        console.log('Invalid credentials');
         return null;
       },
-  })],
+    }),
+  ],
 });
